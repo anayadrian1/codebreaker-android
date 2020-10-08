038250b1bb93788afa8fcf61568ed5b65454b3c3
Adrian Anaya <aanaya79@cnm.edu>
2020-10-08 15:10:51 -0600

    Gutted MainActivity

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -19,41 +19,19 @@ import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements InputFilter {
-
-  private static final String INVALID_CHAR_PATTERN = String.format("[^%s]", MainViewModel.POOL);
-  private static final int[] colorValues =
-      {Color.RED, 0xffffa500, Color.YELLOW, Color.GREEN, Color.BLUE, 0xffee82ee};
-  private static final Map<Character, Integer> colorMap =
-      buildColorMap(MainViewModel.POOL.toCharArray(), colorValues);
+public class MainActivity extends AppCompatActivity {
 
   private MainViewModel viewModel;
-  private GuessAdapter adapter;
-  private int codeLength;
-  private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    binding = ActivityMainBinding.inflate(getLayoutInflater());
-    setContentView(binding.getRoot());
-    setupViews();
+    setContentView(R.layout.activity_main);
     setupViewModel();
   }
 
   private void setupViewModel() {
-    adapter = new GuessAdapter(MainActivity.this, colorMap);
     viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-    viewModel.getGame().observe(this, (game) -> {
-      adapter.clear();
-      adapter.addAll(game.getGuesses());
-      binding.guessList.setAdapter(adapter);
-      binding.guessList.setSelection(adapter.getCount() - 1);
-      codeLength = game.getLength();
-      binding.guess.setText("");
-    });
-    viewModel.getSolved().observe(this, solved ->
-        binding.guessControls.setVisibility(solved ? View.INVISIBLE : View.VISIBLE));
     viewModel.getThrowable().observe(this, (throwable) -> {
       if (throwable != null) {
         Toast.makeText(this, throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show();
@@ -71,13 +49,8 @@ public class MainActivity extends AppCompatActivity implements InputFilter {
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled = true;
+    //noinspection SwitchStatementWithTooFewBranches
     switch (item.getItemId()) {
-      case R.id.new_game:
-        startGame();
-        break;
-      case R.id.restart_game:
-        restartGame();
-        break;
       case R.id.settings:
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
@@ -87,45 +60,4 @@ public class MainActivity extends AppCompatActivity implements InputFilter {
     }
     return handled;
   }
-
-  @Override
-  public CharSequence filter(CharSequence source, int sourceStart, int sourceEnd,
-      Spanned dest, int destStart, int destEnd) {
-    String modifiedSource = source.toString().toUpperCase().replaceAll(INVALID_CHAR_PATTERN, "");
-    StringBuilder builder = new StringBuilder(dest);
-    builder.replace(destStart, destEnd, modifiedSource);
-    if (builder.length() > codeLength) {
-      modifiedSource =
-          modifiedSource.substring(0, modifiedSource.length() - (builder.length() - codeLength));
-    }
-    int newLength = dest.length() - (destEnd - destStart) + modifiedSource.length();
-    binding.submit.setEnabled(newLength == codeLength);
-    return modifiedSource;
-  }
-
-  private void setupViews() {
-    binding.guess.setFilters(new InputFilter[]{this});
-    binding.submit.setOnClickListener(v -> recordGuess());
-  }
-
-  public void recordGuess() {
-    viewModel.guess(binding.guess.getText().toString().trim().toUpperCase());
-  }
-
-  private void startGame() {
-    viewModel.startGame();
-  }
-
-  private void restartGame() {
-    viewModel.restartGame();
-  }
-
-  private static Map<Character, Integer> buildColorMap(char[] chars, int[] values) {
-    Map<Character, Integer> colorMap = new HashMap<>();
-    for (int i = 0; i < chars.length; i++) {
-      colorMap.put(chars[i], values[i]);
-    }
-    return colorMap;
-  }
-
 }
