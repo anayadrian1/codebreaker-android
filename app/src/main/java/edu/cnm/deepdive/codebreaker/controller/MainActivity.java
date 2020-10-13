a84fa9d111bbf00186bfb26a46a5eedc5b1d45a4
Adrian Anaya <aanaya79@cnm.edu>
2020-10-13 10:06:24 -0600

    Starting swatch spinner.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,23 +1,15 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.text.InputFilter;
-import android.text.Spanned;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
-import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
-import edu.cnm.deepdive.codebreaker.databinding.ActivityMainBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
